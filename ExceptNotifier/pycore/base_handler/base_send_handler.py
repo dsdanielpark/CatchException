3bf20f75a9d4a74daf20b817bd3a5f88b1438ce1
parkminwoo <parkminwoo1991@gmail.com>
2023-05-17 03:30:30 +0900

    refactor: set pycore base handler

--- ExceptNotifier/pycore/base_handler/base_send_handler.py
+++ ExceptNotifier/pycore/base_handler/base_send_handler.py
@@ -1,26 +1,9 @@
-from abc import ABC
-import datetime
-from os import environ
-from email.message import EmailMessage
+import abc
+from typing import Any
 
-DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
-
-
-class BaseSendHandler(ABC):
-    def __init__(self) -> None:
-        self.data = None
-
-    def __call__(self, *args):
-        exceptNotifier = EmailMessage()
-        start_time = datetime.datetime.now()
-        f"Time Stamp: {start_time.strftime(DATE_FORMAT)}"
-        exceptNotifier = {
-            "SUBJECT": "[Success Notifier] ** Success! ** Python Code Executed Successfully"
-        }
-        exceptNotifier[
-            "BODY"
-        ] = f"\n\nHi there, \nThis is a success notifier.\n\n - Code Status: Success. \n - Detail: Python Code Ran Without Exceptions. \n - Time: {start_time.strftime(DATE_FORMAT)} \n\nI just wanted to let you know that your Python code has run successfully without any exceptions. \n\nAll the best, \nExcept Notifier github.com/dsdanielpark/ExceptNotifier"
-
-        self.data = {"text": exceptNotifier["SUBJECT"] + exceptNotifier["BODY"]}
-
-    
+class BaseSendHandler(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        """Application sends a success message without any arguments.
+        """
+        pass
