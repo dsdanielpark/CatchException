96e5fbe9cd39de79970c6bbd5aa6c238688495ae
parkminwoo <parkminwoo1991@gmail.com>
2023-05-17 03:44:40 +0900

    refactor: BaseExceptionIpython

---
+++ ExceptNotifier/ipycore/base_handler/base_exception.py
@@ -0,0 +1,15 @@
+import abc
+from typing import Any
+
+class BaseExceptionIpython(metaclass=abc.ABCMeta):
+
+    def __init__(self) -> None:
+        pass
+
+    @abc.abstractmethod
+    def custom_exc(self, *args: Any, **kwds: Any) -> Any:
+        """Application sends a exception message.
+        """
+        pass
+
+    
