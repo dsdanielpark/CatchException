00b37e5ba1b9169e6149b02c7757789b5369bc95
parkminwoo <parkminwoo1991@gmail.com>
2023-05-17 03:31:16 +0900

    refactor: pycore telegram handler

--- ExceptNotifier/pycore/telegram/send_handler.py
+++ ExceptNotifier/pycore/telegram/send_handler.py
@@ -1,15 +1,16 @@
-
-from base_handler.base_send_handler import BaseSendHandler
+# Copyright 2023 parkminwoo
+from typing import Any
+from ExceptNotifier.pycore.base_handler.base_send_handler import BaseSendHandler
 from ExceptNotifier.base.telegram_sender import send_telegram_msg
+from ExceptNotifier.pycore.stacker.send_stacker import stack_send_msg
 from os import environ
 
+
 class SendTelegram(BaseSendHandler):
-    """Sending message to telegram
+    """Sending send message to telegram
     """
-
-    def __init__(self) -> None:
-        super.__init__()
-
-    def __call__(self) -> None:
-        super.__call__()
-        send_telegram_msg(environ["_TELEGRAM_TOKEN"], self.data["text"])
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        data = stack_send_msg('telegram')
+        send_telegram_msg(environ["_TELEGRAM_TOKEN"],data["text"])
+        return super().__call__(*args, **kwds)
+    
--- ExceptNotifier/pycore/telegram/success_handler.py
+++ ExceptNotifier/pycore/telegram/success_handler.py
@@ -1,16 +1,16 @@
-
+# Copyright 2023 parkminwoo
+from typing import Any
 from ExceptNotifier.pycore.base_handler.base_success_handler import BaseSuccessHandler
 from ExceptNotifier.base.telegram_sender import send_telegram_msg
+from ExceptNotifier.pycore.stacker.success_stacker import stack_success_msg
 from os import environ
 
 
 class SuccessTelegram(BaseSuccessHandler):
     """Sending success message to telegram
     """
-
-    def __init__(self) -> None:
-        super.__init__()
-
-    def __call__(self) -> None:
-        super.__call__()
-        send_telegram_msg(environ["_TELEGRAM_TOKEN"], self.data["text"])
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        data = stack_success_msg('telegram')
+        send_telegram_msg(environ["_TELEGRAM_TOKEN"],data["text"])
+        return super().__call__(*args, **kwds)
+    
