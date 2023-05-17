8bd945998298546d4b034902c73b8e273ed84e4e
parkminwoo <parkminwoo1991@gmail.com>
2023-05-17 16:21:05 +0900

    refactor: telegram notifier ipython

--- ExceptNotifier/ipycore/set_telegram/telegram_notifier_ipython.py
+++ ExceptNotifier/ipycore/set_telegram/telegram_notifier_ipython.py
@@ -1,12 +1,10 @@
 # Copyright 2023 parkminwoo
-import datetime
 from os import environ
-from IPython.core.ultratb import AutoFormattedTB
 from ExceptNotifier.base.telegram_sender import send_telegram_msg
 from ExceptNotifier.aicore.openai_receiver import receive_openai_advice
 from ExceptNotifier.aicore.bard_receiver import receive_bard_advice
 from ExceptNotifier.ipycore.base_handler.base_exception import BaseExceptionIpython
-
+from ExceptNotifier.ipycore.stacker.error_stacker import stack_error_msg
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 
@@ -27,24 +25,21 @@ class ExceptTelegramIpython(BaseExceptionIpython):
         :param tb_offset: Offset of traceback, defaults to 1
         :type tb_offset: int, optional
         """
-        
         shell.showtraceback((etype, evalue, tb), tb_offset=tb_offset)
-        
+        data = stack_error_msg(etype, evalue, tb, 'telegram')
         send_telegram_msg(environ["_TELEGRAM_TOKEN"], data["text"])
+
         if environ.get("_OPEN_AI_API") is not None:
             try:
-                
                 advice_msg = receive_openai_advice(
-                    environ["_OPEN_AI_MODEL"], environ["_OPEN_AI_API"], error_message
+                    environ["_OPEN_AI_MODEL"], environ["_OPEN_AI_API"], data['error_message']
                 )
                 send_telegram_msg(environ["_TELEGRAM_TOKEN"], advice_msg)
             except Exception as e:
                 pass
-
         if environ.get("_BARD_API_KEY") is not None:
             try:
-                error_message = f"error sheel=={shell}, error_type_document=={etype.__doc__}, error_value=={evalue}, error message in ipython cell=={sstb}"
-                advice_msg = receive_bard_advice(environ["_BARD_API_KEY"], error_message)
+                advice_msg = receive_bard_advice(environ["_BARD_API_KEY"], data['error_message'])
                 send_telegram_msg(environ["_TELEGRAM_TOKEN"], advice_msg)
             except Exception as e:
                 pass
